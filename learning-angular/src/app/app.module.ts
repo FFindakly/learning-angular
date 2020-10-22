d12a09c51bf0199af5c8fb2ffdb6683e7a92a860
fadi Findakly <fadi.findakly01@stclairconnect.ca>
2020-10-21 23:57:33 -0400

    assignment 3 - not finished

--- learning-angular/src/app/app.module.ts
+++ learning-angular/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ContentCardComponent } from './content-card/content-card.component';
 import { ContentListComponent } from './content-list/content-list.component';
+import { FilterContentPipe } from './filter-content.pipe';
 
 @NgModule({
-  declarations: [AppComponent, ContentCardComponent, ContentListComponent],
+  declarations: [AppComponent, ContentCardComponent, ContentListComponent, FilterContentPipe],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
--- learning-angular/src/app/content-list/content-list.component.ts
+++ learning-angular/src/app/content-list/content-list.component.ts
@@ -52,8 +52,34 @@ export class ContentListComponent implements OnInit {
           'The museum recently moved to a new location at 1105 North University Avenue, in the University of Michigan Biological Sciences Building. ' +
           'It will reopen in April 2019.',
         tags: [' Travel', ' Tourism', ' University', ' Museum', ' Michigan'],
+      },
+      {
+        id: 3,
+        author: 'Fadi Findakly',
+        imageUrl: 'https://www.carmentablog.com/wp-content/uploads/2016/11/troy-movie-images-300x225.jpg',
+        type: 'Movies',
+        title: 'Troy',
+        body:
+          'Troy is a 2004 epic historical war drama film directed by Wolfgang Petersen and written by David Benioff. Produced by units in Malta, Mexico and Britain\'s ' +
+          'Shepperton Studios, the film features an ensemble cast led by Brad Pitt, Eric Bana, and Orlando Bloom.',
+        tags: [' History'],
+      },
+      {
+        id: 4,
+        author: 'Fadi Findakly',
+        imageUrl: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1388185411i/26329._UY475_SS475_.jpg',
+        type: 'Books',
+        title: 'Emotional Intelligence',
+        body:
+          'Emotional Intelligence: Why It Can Matter More Than IQ is a 1995 book by Daniel Goleman. In this book, Goleman posits that ' + 
+           'emotional intelligence is as important as IQ for success, including in academic, professional, social, and interpersonal aspects of one\'s life.',
+        tags: [' Self-Help', 'Psychology'],
       }
     ];
+  }
+
+  getContentsList(): Content[] {
+    return this.contentItemsArray;
   }
 
 }
